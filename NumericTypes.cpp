62dce81f2d4cf25fe9547de924e5d4f71767c510
SeungBeom Kim <64590456+wewe231223@users.noreply.github.com>
2026-02-09 00:42:55 +0900

    NumericTypes SimpleMath 변환 함수 추가

--- NumericTypes.cpp
+++ NumericTypes.cpp
@@ -1,6 +1,8 @@
 #include "NumericTypes.h"
 
 namespace asset {
+    using namespace DirectX::SimpleMath;
+
     Vec2::Vec2()
         : mX{ 0.0f },
 		mY{ 0.0f } {
@@ -69,5 +71,42 @@ namespace asset {
 
     const float* Mat4::Data() const {
         return &mValue[0][0];
+    }
+
+    Vector2 ToSimpleMath(const Vec2& Value) {
+        return Vector2{ Value.mX, Value.mY };
+    }
+
+    Vector3 ToSimpleMath(const Vec3& Value) {
+        return Vector3{ Value.mX, Value.mY, Value.mZ };
+    }
+
+    Vector4 ToSimpleMath(const Vec4& Value) {
+        return Vector4{ Value.mX, Value.mY, Value.mZ, Value.mW };
+    }
+
+    Vector4 ToSimpleMath(const UVec4& Value) {
+        return Vector4{ static_cast<float>(Value.mX), static_cast<float>(Value.mY), static_cast<float>(Value.mZ), static_cast<float>(Value.mW) };
+    }
+
+    Matrix ToSimpleMath(const Mat4& Value) {
+        Matrix result{};
+        result._11 = Value.mValue[0][0];
+        result._12 = Value.mValue[0][1];
+        result._13 = Value.mValue[0][2];
+        result._14 = Value.mValue[0][3];
+        result._21 = Value.mValue[1][0];
+        result._22 = Value.mValue[1][1];
+        result._23 = Value.mValue[1][2];
+        result._24 = Value.mValue[1][3];
+        result._31 = Value.mValue[2][0];
+        result._32 = Value.mValue[2][1];
+        result._33 = Value.mValue[2][2];
+        result._34 = Value.mValue[2][3];
+        result._41 = Value.mValue[3][0];
+        result._42 = Value.mValue[3][1];
+        result._43 = Value.mValue[3][2];
+        result._44 = Value.mValue[3][3];
+        return result;
     }
 }
